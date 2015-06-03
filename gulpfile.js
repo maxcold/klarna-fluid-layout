8ae4b7bb6a3e64b5b7692462e84f31c75de08b9f
Maksim Kholod <mxcold@gmail.com>
2015-06-03 23:00:50 +0300

    stylesheets added

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var uglify = require('gulp-uglify');
 var jshint = require('gulp-jshint');
 var concat = require('gulp-concat');
 var livereload = require('gulp-livereload');
+var minifyCss = require('gulp-minify-css');
 
 gulp.task('minify', function() {
     gulp.src('app/js/*.js')
@@ -11,10 +12,19 @@ gulp.task('minify', function() {
         .pipe(uglify())
         .pipe(concat('result.js'))
         .pipe(gulp.dest('app/build'))
-        .pipe(livereload())
+        .pipe(livereload());
+});
+
+gulp.task('minify-css', function() {
+    gulp.src('app/css/*.css')
+        .pipe(minifyCss())
+        .pipe(concat('result.css'))
+        .pipe(gulp.dest('app/build'))
+        .pipe(livereload());
 });
 
 gulp.task('watch', function() {
     livereload.listen();
     gulp.watch('app/js/*.js', ['minify']);
+    gulp.watch('app/css/*.css', ['minify-css']);
 });
