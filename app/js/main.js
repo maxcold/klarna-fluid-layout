8926f013a21ead07155a0ab1324155ad6e6f0dc5
Maksim Kholod <mxcold@gmail.com>
2015-06-05 00:41:10 +0300

    hover function

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,32 @@
-console.log('test1');
+$(function() {
+    var $document = $(document);
+    var layout = (function() {
+        var $container1 = $('.container1');
+        var $container2 = $('.container2');
+
+        return {
+            init: function() {
+                $document.on('mouseenter', '.box', this.onHover('add'));
+
+                $document.on('mouseleave', '.box', this.onHover('remove'));
+            },
+            onHover: function(metod) {
+
+                return function() {
+                    var $box = $(this);
+                    var hoverElements = $container1.add($container2);
+
+                    hoverElements.add($box).each(function(index, el) {
+                        var $el = $(el);
+
+                        $el[metod + 'Class']('hover');
+                    });
+                }
+            }
+        };
+    })();
+
+    layout.init();
+
+});
+
